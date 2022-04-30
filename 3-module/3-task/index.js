5240968952144c73a6fad4f84f5466fe14afbd33
vtania <vtaniamain@gmail.com>
2022-04-30 21:06:08 +0300

    Done

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  let word = str.split("-");
+
+  if (str.startsWith("-")) {
+    return arrayToPascalCase(word);
+  }
+  return word[0] + arrayToPascalCase(word.slice(1));
+}
+
+function arrayToPascalCase(arr) {
+  return arr.map((el) => el.charAt(0).toUpperCase() + el.slice(1)).join("");
 }
