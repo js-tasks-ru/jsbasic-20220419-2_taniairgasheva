b1d1998b3ad607e2c9ca9a20f5b2fd392dba8f3e
vtania <vtaniamain@gmail.com>
2022-05-25 09:03:08 +0300

    Done

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  let rowsList = table.querySelectorAll("tbody > tr");
+
+  for (let row of rowsList) {
+    let gender = row.children[2];
+    row.classList.add(gender.textContent === "m" ? "male" : "female");
+
+    if (row.children[1].textContent <= 18)
+      row.style.textDecoration = "line-through"; // задача 4
+
+    let status = row.children[3].dataset.available;
+    if (status === "true") {
+      row.classList.add("available");
+    } else if (status === "false") {
+      row.classList.add("unavailable");
+    } else {
+      row.hidden = true;
+    }
+  }
 }
