532fea2dac716b60c39eaf03ca4966b7fc50a932
vtania <vtaniamain@gmail.com>
2022-05-26 22:26:07 +0300

    Done

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  const container = document.querySelector(".carousel__inner");
+  const goNext = document.querySelector(".carousel__arrow_right");
+  const goBack = document.querySelector(".carousel__arrow_left");
+
+  let index = 0;
+  goBack.style.display = "none";
+
+  function updateButtonsVisibility() {
+    if (index > 0) {
+      goBack.style.display = "";
+    } else {
+      goBack.style.display = "none";
+    }
+    if (index === container.children.length - 1) {
+      goNext.style.display = "none";
+    } else {
+      goNext.style.display = "";
+    }
+  }
+
+  goNext.addEventListener("click", () => {
+    if (index < container.children.length - 1) {
+      index += 1;
+      value = `${container.offsetWidth * index}px`;
+      container.style.transform = `translateX(-${value})`;
+    }
+    updateButtonsVisibility();
+  });
+
+  goBack.addEventListener("click", () => {
+    if (index > 0) {
+      index -= 1;
+      value = `${container.offsetWidth * index}px`;
+      container.style.transform = `translateX(-${value})`;
+    }
+    updateButtonsVisibility();
+  });
 }
